ca67556b23a0962434a960155e625689724dd25e
Karim Luy <karim.luy@he-arc.ch>
2016-12-29 16:18:32 +0100

    updated nav bar and created mouse over logic in js, closed issue #1

--- App/js/Controller/MainController.js
+++ App/js/Controller/MainController.js
@@ -1,24 +1,36 @@
 
 var today;
+var pages = document.getElementById('Pages').childNodes;
 
-$(function () {
-	today = m_getToday();
-	m_initEventListeners();
-});
+// $(function () {
+// 	today = m_getToday();
+// 	m_initEventListeners();
+// });
 
-function m_initEventListeners() {
-	
-}
+// function m_initEventListeners() {
+//
+// }
 
-function m_getToday() {
-    let today = new Date();
-    let day = days[today.getDay()];
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1; //January is 0!
-    let yyyy = today.getFullYear();
+// function m_getToday() {
+//     var today = new Date();
+//     var day = days[today.getDay()];
+//     var dd = today.getDate();
+//     var mm = today.getMonth() + 1; //January is 0!
+//     var yyyy = today.getFullYear();
+//
+//     if (dd < 10) dd = '0' + dd;
+//     if (mm < 10) mm = '0' + mm;
+//
+//     return day + ', ' + mm + '.' + dd + '.' + yyyy;
+// }
 
-    if (dd < 10) dd = '0' + dd;
-    if (mm < 10) mm = '0' + mm;
-
-    return day + ', ' + mm + '.' + dd + '.' + yyyy;
+function hideUnhidePage(page){
+    for(var i = 1; i < pages.length; i+=2){
+        if (pages.item(i) == pages.item(page)){
+            pages.item(i).style.visibility = 'visible';
+        }
+        else{
+            pages.item(i).style.visibility = 'hidden';
+        }
+    }
 }
